f82b434e9402425975a581a5f84293666ef53b3b
Mathan <mathanmahe99@gmail.com>
2025-10-04 00:00:02 -0700

    Update directory for workerclient and perform null checks in videoDetector

--- frontend/src/utils/worker-client.js
+++ frontend/src/utils/worker-client.js
@@ -4,42 +4,49 @@ importScripts("https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js");
 ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/";
 
 const sessionOptions = {
-    executionProviders: ['cpu'], // Prioritize GPU (WebGL), then WASM, then CPU
+  executionProviders: ["cpu"], // Prioritize GPU (WebGL), then WASM, then CPU
 };
 
 if (typeof SharedArrayBuffer !== "undefined") {
-    ort.env.wasm.numThreads = 4; // Use multi-threading if supported
-    console.log("Multithread supported");
+  ort.env.wasm.numThreads = 4; // Use multi-threading if supported
+  console.log("Multithread supported");
 } else {
-    console.warn("SharedArrayBuffer not supported. Falling back to single-threaded execution.");
+  console.warn(
+    "SharedArrayBuffer not supported. Falling back to single-threaded execution."
+  );
 }
 
 if (ort.env.wasm.simd) {
-    ort.env.wasm.simd = true; // Enable SIMD if supported
-    console.log("SIMD supported in this browser.");
+  ort.env.wasm.simd = true; // Enable SIMD if supported
+  console.log("SIMD supported in this browser.");
 } else {
-    console.warn("SIMD not supported in this browser.");
+  console.warn("SIMD not supported in this browser.");
 }
 
 let sessionPromise;
 function getSession() {
   if (!sessionPromise) {
-    sessionPromise = ort.InferenceSession.create("/models/yolov11n_bestFire.onnx", sessionOptions);
+    sessionPromise = ort.InferenceSession.create(
+      "/yolov11n_bestFire.onnx",
+      sessionOptions
+    );
   }
   return sessionPromise;
 }
 
 onmessage = async (event) => {
   const { type, data, dims } = event.data || {};
-  if (type !== 'infer' || !data) return;
+  if (type !== "infer" || !data) return;
 
   try {
     const session = await getSession();
-    const tensor = new ort.Tensor(new Float32Array(data), dims || [1,3,640,640]);
+    const tensor = new ort.Tensor(
+      new Float32Array(data),
+      dims || [1, 3, 640, 640]
+    );
     const outputs = await session.run({ images: tensor });
     postMessage(outputs["output0"].data);
   } catch (err) {
     console.error("Error during inference:", err);
   }
 };
-
